18e260d2053fc9d52f1841bc4636d217acf6b4b3
Primael BANKOLE <bankolewilli@gmail.com>
2025-07-23 20:45:18 +0100

    Produit

--- resources/views/stand_produits.blade.php
+++ resources/views/stand_produits.blade.php
@@ -17,9 +17,9 @@
     @if($stand->produits->isEmpty())
         <p>Aucun produit disponible pour ce stand.</p>
     @else
-        <div class="produits-list">
+        <div  class="produits-list">
             @foreach($stand->produits as $produit)
-                <div class="produit-card">
+                <div style="margin-top: 70px; width: 450px; justify-content: space-between; border: solid 1px black; padding: 20px; border-radius: 10px; background-color: #fffffd; box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1) ;" class="produit-card">
                     <h2>{{ $produit->nom }}</h2>
                     <p>Description : {{ $produit->description }}</p>
                     <p>Prix : {{ $produit->prix }} €</p>
